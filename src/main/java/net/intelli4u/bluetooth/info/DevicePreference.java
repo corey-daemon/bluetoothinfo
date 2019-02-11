bbdf058b2507866b898809945fa4ebd744343fd9
ccc <cadappl@gmail.com>
2019-02-11 15:38:33 +0800

    device: add drawable for major class audio/video

--- src/main/java/net/intelli4u/bluetooth/info/DevicePreference.java
+++ src/main/java/net/intelli4u/bluetooth/info/DevicePreference.java
@@ -63,6 +63,8 @@ public class DevicePreference extends Preference implements Preference.OnPrefere
                 return R.drawable.ic_bt_imaging;
             case BluetoothClass.Device.Major.NETWORKING:
                 return R.drawable.ic_bt_network_pan;
+            case BluetoothClass.Device.Major.AUDIO_VIDEO:
+                return R.drawable.ic_bt_headphones_a2dp;
             case BluetoothClass.Device.Major.PERIPHERAL:
                 return R.drawable.ic_bt_keyboard_hid;
             default:
